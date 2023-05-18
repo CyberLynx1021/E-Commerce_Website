eb6df176283f0a7a6807aa2504e080fbc7c2af7e
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-17 23:29:49 -0500

    E commerce with JS 12:44 mins

--- books.js
+++ books.js
@@ -4,10 +4,10 @@ function renderBooks() {
   const booksHtml = books.map((book) => {
    return  `<div class="book">
     <figure class="book__img--wrapper">
-      <img class="book__img" src="${books[0].url}" alt="">
+      <img class="book__img" src="${book.url}" alt="">
     </figure>
     <div class="book__title">
-    ${books[0].title}
+    ${books.title}
     </div>
     <div class="book__ratings">
       <i class="fas fa-star"></i>
@@ -17,7 +17,7 @@ function renderBooks() {
       <i class="fas fa-star-half-alt"></i>
     </div>
     <div class="book__price">
-      <span class="book__price--normal">$${books[0].originalPrice}</span>$${books[0].salePrice} 
+      <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice} 
     </div>
   </div>`;
   });
