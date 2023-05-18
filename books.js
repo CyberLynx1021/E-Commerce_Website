37d81d696e607f63b3fac19bf2671187b6915853
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-17 20:40:24 -0500

    E commerce with JS 12:44 min

--- books.js
+++ books.js
@@ -1,13 +1,13 @@
 function renderBooks() {
-  const booksWrapper = document.querySelector('.books')
-  
-  booksWrapper.innerHTML = 
-    `<div class="book">
+  const booksWrapper = document.querySelector('.books');
+  const books = getBooks();
+  const booksHtml = books.map((book) => {
+   return  `<div class="book">
     <figure class="book__img--wrapper">
-      <img class="book__img" src="./asset/Assets/deep work.jpeg" alt="">
+      <img class="book__img" src="${books[0].url}" alt="">
     </figure>
     <div class="book__title">
-      Deep Work
+    ${books[0].title}
     </div>
     <div class="book__ratings">
       <i class="fas fa-star"></i>
@@ -17,10 +17,15 @@ function renderBooks() {
       <i class="fas fa-star-half-alt"></i>
     </div>
     <div class="book__price">
-      <span class="book__price--normal">$59.95</span> $14.95
+      <span class="book__price--normal">$${books[0].originalPrice}</span>$${books[0].salePrice} 
     </div>
-  </div>`
-      }
+  </div>`;
+  });
+
+  booksWrapper.innerHTML = booksHtml;
+  console.log(booksHtml)
+      // booksWrapper.innerHTML ;
+}
 
     setTimeout(() => {
       renderBooks();
@@ -34,7 +39,7 @@ function renderBooks() {
             {
               id: 1,
               title: "Crack the Coding Interview",
-              url: "assets/crack the coding interview.png",
+              url: "asset/assets/crack the coding interview.png",
               originalPrice: 49.95,
               salePrice: 14.95,
               rating: 4.5,
@@ -42,7 +47,7 @@ function renderBooks() {
             {
               id: 2,
               title: "Atomic Habits",
-              url: "assets/atomic habits.jpg",
+              url: "asset/assets/atomic habits.jpg",
               originalPrice: 39,
               salePrice: null,
               rating: 5,
@@ -50,7 +55,7 @@ function renderBooks() {
             {
               id: 3,
               title: "Deep Work",
-              url: "assets/deep work.jpeg",
+              url: "asset/assets/deep work.jpeg",
               originalPrice: 29,
               salePrice: 12,
               rating: 5,
@@ -58,7 +63,7 @@ function renderBooks() {
             {
               id: 4,
               title: "The 10X Rule",
-              url: "assets/book-1.jpeg",
+              url: "asset/assets/book-1.jpeg",
               originalPrice: 44,
               salePrice: 19,
               rating: 4.5,
@@ -66,7 +71,7 @@ function renderBooks() {
             {
               id: 5,
               title: "Be Obsessed Or Be Average",
-              url: "assets/book-2.jpeg",
+              url: "asset/assets/book-2.jpeg",
               originalPrice: 32,
               salePrice: 17,
               rating: 4,
@@ -74,7 +79,7 @@ function renderBooks() {
             {
               id: 6,
               title: "Rich Dad Poor Dad",
-              url: "assets/book-3.jpeg",
+              url: "asset/assets/book-3.jpeg",
               originalPrice: 70,
               salePrice: 12.5,
               rating: 5,
@@ -82,7 +87,7 @@ function renderBooks() {
             {
               id: 7,
               title: "Cashflow Quadrant",
-              url: "assets/book-4.jpeg",
+              url: "asset/assets/book-4.jpeg",
               originalPrice: 11,
               salePrice: 10,
               rating: 4,
@@ -90,7 +95,7 @@ function renderBooks() {
             {
               id: 8,
               title: "48 Laws of Power",
-              url: "assets/book-5.jpeg",
+              url: "asset/assets/book-5.jpeg",
               originalPrice: 38,
               salePrice: 17.95,
               rating: 4.5,
@@ -98,7 +103,7 @@ function renderBooks() {
             {
               id: 9,
               title: "The 5 Second Rule",
-              url: "assets/book-6.jpeg",
+              url: "asset/assets/book-6.jpeg",
               originalPrice: 35,
               salePrice: null,
               rating: 2,
@@ -106,7 +111,7 @@ function renderBooks() {
             {
               id: 10,
               title: "Your Next Five Moves",
-              url: "assets/book-7.jpg",
+              url: "asset/assets/book-7.jpg",
               originalPrice: 40,
               salePrice: null,
               rating: 4,
@@ -114,7 +119,7 @@ function renderBooks() {
             {
               id: 11,
               title: "Mastery",
-              url: "assets/book-8.jpeg",
+              url: "asset/assets/book-8.jpeg",
               originalPrice: 30,
               salePrice: null,
               rating: 4.5,
