edee532651ef6ae938902530c9ded017a67451a3
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-19 19:17:02 -0500

    E commerce with JS 38:01 minutes

--- books.js
+++ books.js
@@ -1,38 +1,55 @@
 async function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
-
    const books = await getBooks()
+  if(filter === 'LOW_TO_HIGH'){
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+  } else if (filter === 'HIGH_TO_LOW'){
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  } else if (filter === 'RATING'){
+    books.sort((a, b) => b.rating - a.rating);
+  }
 
-   
-   if(filter === 'LOW_TO_HIGH'){
-    console.log(filter)
-   }
-
-
-    const booksHtml = books?.map((book) => {
+   const booksHtml = 
+   books?.map((book) => {
     return `<div class="book">
     <figure class="book__img--wrapper">
       <img class="book__img" src="${book.url}" alt="">
     </figure>
     <div class="book__title">
-    ${books.title}
+    ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span>$${book.originalPrice.toFixed(2)}</span> 
+    ${priceHTML(book.orignialPrice, book.salePrice)}
     </div>
   </div>`;
   })
-  
   .join("");
   
   booksWrapper.innerHTML = booksHtml;
+}
+
+async function priceHTML(originalPrice, salePrice){
+  if(!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+ else {
+   return `<span class="book__price--normal">$${originalPrice}</span> $${salePrice}`
+   }
+  }
+
+
+ function ratingsHTML(rating){
+  let ratingHTML = "";
+  for(let i = 0; i < Math.floor(rating); ++i){
+  ratingHTML += '<i class="fas fa-star"></i>\n';
+}
+if (!Number.isInteger(rating)){
+  ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`
+}
+return ratingHTML;
 }
 
  function filterBooks(event){
